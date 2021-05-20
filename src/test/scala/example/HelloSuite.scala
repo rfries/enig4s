93ffe7f869d3706045fe9dbc50cba42ef950233b
Robert Fries <robert@robertfries.com>
2021-05-19 23:54:10 -0400

    Add some Position tests

--- src/test/scala/example/HelloSuite.scala
+++
@@ -1,7 +0,0 @@
-package example
-
-class HelloSuite extends munit.FunSuite {
-  test("The Hello object should say hello.") {
-    assertEquals(Hello.greeting, "hello")
-  }
-}
--- src/test/scala/org/somecode/enigma/mach/PositionPropSuite.scala
+++ src/test/scala/org/somecode/enigma/mach/PositionPropSuite.scala
@@ -10,9 +10,23 @@ given posGen: Gen[Position] = posValGen.map(Position.unsafe)
 
 class PositionPropSuite extends ScalaCheckSuite:
 
+  test("Creation") {
+    assert(Position(0).isInstanceOf[Right[String, Position]])
+    assert(Position(2).isInstanceOf[Right[String, Position]])
+    assert(Position(Position.Max-1).isInstanceOf[Right[String, Position]])
+    assert(Position(Position.Max).isInstanceOf[Left[String, Position]])
+    intercept[java.lang.IllegalArgumentException] {
+      Position.unsafe(Position.Max)
+    }
+  }
+
+  test("Position.next") {
+    assertEquals(Position(0).require.next.value, 1)
+    assertEquals(Position(Position.Max-1).require.next.value, 0)
+  }
+
   property("Adding Positions") {
     forAll (posGen, posGen) { (p1: Position, p2: Position) =>
       assert((p1 + p2).value == (p1.value + p2.value) % Position.Max)
     }
   }
-
