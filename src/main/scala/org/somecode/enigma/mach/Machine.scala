f546bb6ea0312b506dfd900ca06b244e99321fda
Robert Fries <robert@funobjects.io>
2021-08-03 03:56:07 -0400

    Machine.apply

--- src/main/scala/org/somecode/enigma/mach/Machine.scala
+++ src/main/scala/org/somecode/enigma/mach/Machine.scala
@@ -2,20 +2,28 @@ package org.somecode.enigma
 package mach
 
 import cats.data.State
+import Machine.Rotor
 
 case class Machine private (
-  wheels: Vector[Wheel],
-  ringSettings: Vector[KeyCode],
-  reflector: Wheel,
-  initialSettings: Machine.MachineState)
+  wheels: Vector[ConfiguredWheel],
+  reflector: Rotor,
+  init: Machine.MachineState):
+
+  def size: Int = reflector.size
 
 object Machine:
 
-  def apply (
-    wheels: Vector[Wheel],
-    ringSettings: Vector[KeyCode],
-    reflector: Wheel,
-    initialSettings: MachineState): Either[String, Machine] = ???
+  def apply (wheels: Vector[ConfiguredWheel],
+             reflector: Rotor,
+             init: MachineState): Either[String, Machine] =
+    if wheels.exists(_.size != reflector.size) then
+      Left("Wheel size must match reflector size.")
+    else if wheels.size != init.wheelState.size then
+      Left(s"Initial state vector size (${init.wheelState.size}) must match the wheel vector size (${wheels.size}).")
+    else if init.wheelState.exists(_.position >= reflector.size) then
+      Left(s"Initial state vector must contain only values less than ${reflector.size}.")
+    else
+      Right(new Machine(wheels, reflector, init))
 
   final case class MachineState(wheelState: Vector[WheelState])
   final case class WheelState(position: KeyCode)
