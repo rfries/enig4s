627956ce1e106b3011a333490172a8fa018ea31b
Robert Fries <robert@robertfries.com>
2023-03-25 04:05:07 -0400

    Use ResourceApp

--- server/src/main/scala/org/somecode/enig4s/server/Main.scala
+++ server/src/main/scala/org/somecode/enig4s/server/Main.scala
@@ -1,45 +1,46 @@
 package org.somecode.enig4s.server
 
 import cats.effect.*
+import cats.effect.implicits.*
 import cats.implicits.*
-import com.comcast.ip4s._
+import com.comcast.ip4s.*
 import fs2.concurrent.SignallingRef
 import fs2.Stream
 import org.http4s.HttpRoutes
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
-import org.http4s.server.Router
+import org.http4s.server.{Router, Server}
 import org.http4s.server.staticcontent.{FileService, fileService}
 import org.somecode.enig4s.mach.Cabinet
 import org.somecode.enig4s.server.service.MachineService
 import org.somecode.enig4s.server.service.MetaService
+import cats.Applicative
 
-import scala.concurrent.ExecutionContext.global
+object Main extends ResourceApp:
 
-object Main extends IOApp:
-
-  override def run(args: List[String]): IO[ExitCode] =
-    serverStream[IO].compile.lastOrError
-
-  private[server] def serverStream[F[_]: Async]: Stream[F, ExitCode] =
+  override def run(args: List[String]): Resource[IO, ExitCode] =
     for
-      // create the shutdown signal and exit status ref (success if not changed)
-      shutdown  <- Stream.eval(SignallingRef[F, Boolean](false))
-      exitCode  <- Stream.eval(Ref[F].of(ExitCode.Success))
-      // initialize pre-defined objects
-      cabinet   <- Stream.fromEither(Cabinet().left.map(s => new java.lang.IllegalArgumentException(s)))
-      // create the server
-      serverRes = Stream.resource(EmberServerBuilder
-                    .default[F]
-                    .withHost(ipv4"0.0.0.0")
-                    .withPort(port"8080")
-                    .withHttpApp(routes[F](shutdown, cabinet).orNotFound)
-                    .build)
-      // emulate serveWhile from BlazeServerBuilder (so endpoint can signal shutdown)
-      server    <- serverRes
-                    *> (shutdown.discrete.takeWhile(_ === false).drain
-                      ++ Stream.eval(exitCode.get))
-    yield server
+      shutdown <- serverResource[IO]
+      _ <- shutdownResource(shutdown)
+    yield ExitCode.Success
+
+  def cabResource[F[_]](using F: Async[F]): Resource[F, Cabinet] =
+    Resource.liftK(F.fromEither(Cabinet().leftMap(s => new IllegalStateException(s))))
+
+  def shutdownResource[F[_]: Async](ref: SignallingRef[F, Boolean]): Resource[F, Option[Boolean]] =
+    ref.discrete.takeWhile(_ === false).drain.compile.resource.last
+
+  def serverResource[F[_]: Async]: Resource[F, SignallingRef[F, Boolean]] =
+    for
+      shutdown  <- Resource.liftK(SignallingRef[F, Boolean](false))
+      cabinet   <- cabResource
+      _ <- EmberServerBuilder
+        .default[F]
+        .withHost(ipv4"0.0.0.0")
+        .withPort(port"8080")
+        .withHttpApp(routes[F](shutdown, cabinet).orNotFound)
+        .build
+    yield shutdown
 
   private[server] def routes[F[_]: Async](shutdown: SignallingRef[F, Boolean], cabinet: Cabinet): HttpRoutes[F] =
     Router(
